e413e64ec5e40c02e320830fae218375f0c85243
CardinalR <raph_7845@hotmail.com>
2016-04-25 09:00:57 -0400

    Bruh

---
+++ ProjetFinal Prototype Interface/FormAjouterJoueur.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace ProjetFinal_Prototype_Interface
+{
+    partial class FormAjouterJoueur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPong = new System.Windows.Forms.Label();
+            this.labelNom = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelPrenom = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelCourriel = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.labelDateNaissance = new System.Windows.Forms.Label();
+            this.labelAnnee = new System.Windows.Forms.Label();
+            this.labelMois = new System.Windows.Forms.Label();
+            this.labelJour = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.labelNumTelephone = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.labelPhoto = new System.Windows.Forms.Label();
+            this.buttonInscrire = new System.Windows.Forms.Button();
+            this.buttonAnnuler = new System.Windows.Forms.Button();
+            this.buttonChoisirPhoto = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelPong
+            // 
+            this.labelPong.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelPong.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPong.ForeColor = System.Drawing.Color.Yellow;
+            this.labelPong.Location = new System.Drawing.Point(-2, -1);
+            this.labelPong.Name = "labelPong";
+            this.labelPong.Size = new System.Drawing.Size(287, 39);
+            this.labelPong.TabIndex = 2;
+            this.labelPong.Text = "PONG!";
+            this.labelPong.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelNom
+            // 
+            this.labelNom.AutoSize = true;
+            this.labelNom.Location = new System.Drawing.Point(116, 53);
+            this.labelNom.Name = "labelNom";
+            this.labelNom.Size = new System.Drawing.Size(32, 13);
+            this.labelNom.TabIndex = 3;
+            this.labelNom.Text = "Nom:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // labelPrenom
+            // 
+            this.labelPrenom.AutoSize = true;
+            this.labelPrenom.Location = new System.Drawing.Point(0, 53);
+            this.labelPrenom.Name = "labelPrenom";
+            this.labelPrenom.Size = new System.Drawing.Size(46, 13);
+            this.labelPrenom.TabIndex = 5;
+            this.labelPrenom.Text = "Prénom:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // labelCourriel
+            // 
+            this.labelCourriel.AutoSize = true;
+            this.labelCourriel.Location = new System.Drawing.Point(0, 104);
+            this.labelCourriel.Name = "labelCourriel";
+            this.labelCourriel.Size = new System.Drawing.Size(45, 13);
+            this.labelCourriel.TabIndex = 7;
+            this.labelCourriel.Text = "Courriel:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(3, 117);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(240, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // labelDateNaissance
+            // 
+            this.labelDateNaissance.AutoSize = true;
+            this.labelDateNaissance.Location = new System.Drawing.Point(0, 148);
+            this.labelDateNaissance.Name = "labelDateNaissance";
+            this.labelDateNaissance.Size = new System.Drawing.Size(99, 13);
+            this.labelDateNaissance.TabIndex = 9;
+            this.labelDateNaissance.Text = "Date de naissance:";
+            // 
+            // labelAnnee
+            // 
+            this.labelAnnee.AutoSize = true;
+            this.labelAnnee.Location = new System.Drawing.Point(0, 161);
+            this.labelAnnee.Name = "labelAnnee";
+            this.labelAnnee.Size = new System.Drawing.Size(41, 13);
+            this.labelAnnee.TabIndex = 10;
+            this.labelAnnee.Text = "Année:";
+            // 
+            // labelMois
+            // 
+            this.labelMois.AutoSize = true;
+            this.labelMois.Location = new System.Drawing.Point(47, 161);
+            this.labelMois.Name = "labelMois";
+            this.labelMois.Size = new System.Drawing.Size(32, 13);
+            this.labelMois.TabIndex = 11;
+            this.labelMois.Text = "Mois:";
+            // 
+            // labelJour
+            // 
+            this.labelJour.AutoSize = true;
+            this.labelJour.Location = new System.Drawing.Point(95, 161);
+            this.labelJour.Name = "labelJour";
+            this.labelJour.Size = new System.Drawing.Size(30, 13);
+            this.labelJour.TabIndex = 12;
+            this.labelJour.Text = "Jour:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(3, 177);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown1.TabIndex = 13;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(98, 177);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown2.TabIndex = 14;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(50, 177);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown3.TabIndex = 15;
+            // 
+            // labelNumTelephone
+            // 
+            this.labelNumTelephone.AutoSize = true;
+            this.labelNumTelephone.Location = new System.Drawing.Point(0, 200);
+            this.labelNumTelephone.Name = "labelNumTelephone";
+            this.labelNumTelephone.Size = new System.Drawing.Size(112, 13);
+            this.labelNumTelephone.TabIndex = 16;
+            this.labelNumTelephone.Text = "Numéro de téléphone:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(3, 216);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(240, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // labelPhoto
+            // 
+            this.labelPhoto.AutoSize = true;
+            this.labelPhoto.Location = new System.Drawing.Point(0, 256);
+            this.labelPhoto.Name = "labelPhoto";
+            this.labelPhoto.Size = new System.Drawing.Size(41, 13);
+            this.labelPhoto.TabIndex = 18;
+            this.labelPhoto.Text = "Photo: ";
+            // 
+            // buttonInscrire
+            // 
+            this.buttonInscrire.Location = new System.Drawing.Point(12, 284);
+            this.buttonInscrire.Name = "buttonInscrire";
+            this.buttonInscrire.Size = new System.Drawing.Size(122, 23);
+            this.buttonInscrire.TabIndex = 19;
+            this.buttonInscrire.Text = "S\'inscrire";
+            this.buttonInscrire.UseVisualStyleBackColor = true;
+            // 
+            // buttonAnnuler
+            // 
+            this.buttonAnnuler.Location = new System.Drawing.Point(150, 284);
+            this.buttonAnnuler.Name = "buttonAnnuler";
+            this.buttonAnnuler.Size = new System.Drawing.Size(122, 23);
+            this.buttonAnnuler.TabIndex = 20;
+            this.buttonAnnuler.Text = "Annuler";
+            this.buttonAnnuler.UseVisualStyleBackColor = true;
+            this.buttonAnnuler.Click += new System.EventHandler(this.buttonAnnuler_Click);
+            // 
+            // buttonChoisirPhoto
+            // 
+            this.buttonChoisirPhoto.Location = new System.Drawing.Point(50, 251);
+            this.buttonChoisirPhoto.Name = "buttonChoisirPhoto";
+            this.buttonChoisirPhoto.Size = new System.Drawing.Size(84, 23);
+            this.buttonChoisirPhoto.TabIndex = 21;
+            this.buttonChoisirPhoto.Text = "Sélectionner";
+            this.buttonChoisirPhoto.UseVisualStyleBackColor = true;
+            // 
+            // FormAjouterJoueur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 319);
+            this.Controls.Add(this.buttonChoisirPhoto);
+            this.Controls.Add(this.buttonAnnuler);
+            this.Controls.Add(this.buttonInscrire);
+            this.Controls.Add(this.labelPhoto);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.labelNumTelephone);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.labelJour);
+            this.Controls.Add(this.labelMois);
+            this.Controls.Add(this.labelAnnee);
+            this.Controls.Add(this.labelDateNaissance);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.labelCourriel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.labelPrenom);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelNom);
+            this.Controls.Add(this.labelPong);
+            this.Name = "FormAjouterJoueur";
+            this.Text = "S\'inscrire";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPong;
+        private System.Windows.Forms.Label labelNom;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelPrenom;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelCourriel;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label labelDateNaissance;
+        private System.Windows.Forms.Label labelAnnee;
+        private System.Windows.Forms.Label labelMois;
+        private System.Windows.Forms.Label labelJour;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label labelNumTelephone;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label labelPhoto;
+        private System.Windows.Forms.Button buttonInscrire;
+        private System.Windows.Forms.Button buttonAnnuler;
+        private System.Windows.Forms.Button buttonChoisirPhoto;
+    }
+}
